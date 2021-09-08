17db71138be226034d7abf46ba91c91d3bc00a95
VRoman01 <venchak.201130@gmail.com>
2021-09-08 11:45:04 +0300

    create power_lin_gen and print it)

--- fun_gen.py
+++ fun_gen.py
@@ -3,13 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def lin_fun_gen(n_dots: int, x_scale: tuple, k: int, b: int, std: int):
-    rng = np.random.RandomState(1)
-    x = x_scale[0] +(x_scale[1]-x_scale[0]) * rng.rand(n_dots)
-    y = k * x + b + std*rng.randn(n_dots)
-    return x, y
-
-
 def approx(x, y):
     n = x.size
     sum_x = x.sum()
@@ -21,22 +14,45 @@ def approx(x, y):
     return k, b
 
 
+def lin_fun_gen(n_dots: int, x_scale: tuple, k: int, b: int, std: int):
+    rng = np.random.RandomState(1)
+    x = x_scale[0] +(x_scale[1]-x_scale[0]) * rng.rand(n_dots)
+    y = k * x + b + std*rng.randn(n_dots)
+    return x, y
+
+
 def draw(x, y, name, inf):
     fig = plt.figure()
     axe = fig.add_subplot()
-    axe.scatter(x, y, c='g', label='k={} b={} n={} scale={}, std={}'.format(inf['k'],
-                                                                            inf['b'],
-                                                                            inf['n_dots'],
-                                                                            inf['x_scale'],
-                                                                            inf['std']))
+    axe.scatter(x, y, c='g', label=str(inf))
     k, b = approx(x, y)
     y_approx = k*x + b
     axe.plot(x, y_approx, c='r', label='k={:2.1f} b={:2.1f}'.format(k, b))
     axe.legend()
-    plt.savefig('image/{}.png'.format(name))
+    plt.savefig('image/{}'.format(name))
+
+
+def power_lin_fun_gen(n_dots: int, x_scale: tuple, w, std: int):
+    rng = np.random.RandomState(1)
+    x = x_scale[0] + (x_scale[1] - x_scale[0]) * rng.rand(n_dots)
+    X = np.array([np.ones(n_dots), x]).T
+    Y = np.matmul(X,np.array(w)) + std*rng.randn(n_dots)
+    return X, Y
+
+
+def power_draw(X, Y, name, inf):
+    fig = plt.figure()
+    axe = fig.add_subplot()
+    axe.scatter(X.T[1], Y, c='g', label=str(inf))
+    axe.legend()
+    plt.savefig('image/{}'.format(name))
 
 
 if __name__ == '__main__':
+    inf = {'n_dots': 50, 'x_scale': (-10, 10), 'w': [5, 3], 'std': 3}
+    X, Y = power_lin_fun_gen(**inf)
+    power_draw(X, Y, name='second', inf=inf)
+
     inf = {'n_dots': 50, 'x_scale': (-10, 10), 'k': 2, 'b': 5, 'std': 3}
     x, y = lin_fun_gen(**inf)
     draw(x, y, 'first', inf)
