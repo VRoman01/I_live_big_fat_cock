8919b1cd0d22e467a3081f38da19fa21793be241
VRoman01 <venchak.201130@gmail.com>
2021-09-13 11:00:58 +0300

    create analytical_solution

--- fun_gen.py
+++ fun_gen.py
@@ -35,10 +35,23 @@ def draw(x, y, name, inf):
 
 def power_lin_fun_gen(n_dots: int, x_scale: tuple, w, std: int):
     rng = np.random.RandomState(1)
+    # Training data
     x = x_scale[0] + (x_scale[1] - x_scale[0]) * rng.rand(n_dots)
-    X = np.array([np.ones(n_dots), x]).T
-    Y = np.matmul(X,np.array(w)) + std*rng.randn(n_dots)
-    return X, Y
+    X_train = np.array([np.ones(n_dots), x]).T
+    Y_train = np.matmul(X_train,np.array(w)) + std*rng.randn(n_dots)
+    # Test data
+    x = np.arange(x_scale[0], x_scale[1], (x_scale[1]-x_scale[0])/n_dots)
+    X_test = np.array([np.ones(n_dots), x]).T
+    Y_test = np.matmul(X_test, np.array(w))
+    return X_train, Y_train, X_test, Y_test
+
+
+def analytical_solution(X, Y):
+    try:
+        w = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+        return w
+    except np.linalg.LinAlgError as e:
+        print(e)
 
 
 def power_draw(X, Y, name, inf):
@@ -54,10 +67,17 @@ def power_draw(X, Y, name, inf):
 
 
 if __name__ == '__main__':
-    inf = {'n_dots': 50, 'x_scale': (-10, 10), 'w': [5, 3], 'std': 3}
-    X, Y = power_lin_fun_gen(**inf)
-    power_draw(X, Y, name='second', inf=inf)
+    inf = {'n_dots': 50,
+           'x_scale': (-10, 10),
+           'w': [5, 3],
+           'std': 3,
+           }
+    X_train, Y_train, X_test, Y_test = power_lin_fun_gen(**inf)
+    # power_draw(X_train, Y_train, name='second', inf=inf)
+    w = analytical_solution(X_train, Y_train)
+    print(w)
 
-    inf = {'n_dots': 50, 'x_scale': (-10, 10), 'k': 2, 'b': 5, 'std': 3}
-    x, y = lin_fun_gen(**inf)
-    draw(x, y, 'first', inf)
+
+    # inf = {'n_dots': 50, 'x_scale': (-10, 10), 'k': 2, 'b': 5, 'std': 3}
+    # x, y = lin_fun_gen(**inf)
+    # draw(x, y, 'first', inf)
